00386a1d56f4fc6289ce2b570b94afb9d60b2d50
Nicholas Geyer <geyer@atmos.colostate.edu>
2016-10-05 11:44:53 -0600

    Updated ScalingRegression.py

--- ScalingRegression.py
+++ ScalingRegression.py
@@ -1,124 +1,114 @@
 #! /Library/Frameworks/Python.framework/Versions/anaconda/bin/python
 
-class unit():
-	def __init__(self,value,grad):
-		self.value = value
-		self.grad = grad
-class multiply_gate():
-	def __init__(self):
-		print 'multiply_gate initialized'
-		
-	def forward(self,u0,u1):
-		self.u0 = u0
-		self.u1 = u1
-		self.utop = unit(u0.value*u1.value,0.)
-		return self.utop
-	
-	def backward(self):
-		self.u0.grad += self.u1.value * self.utop.grad
-		self.u1.grad += self.u0.value * self.utop.grad
+#=====================================================================#
+# This is a basic polynomial regression prototype.  It is basic and   #
+#	uses the stock market pricing of a random ticker.   The dataset   #
+#	is oriented in time from most recent to the first price.          #
+#	This is just to organize a new algorithm for some other project.  #
+#	In general, it will make a random training set and CV set and     #
+#	will find the best parameter fit over many different realizations #
+#	This technique is similar to Monte Carlo/K-folds and bootstrapping#
+#		Created by: nmg5038											  #
+#		Created on: 10/5/16											  #
+#=====================================================================#
 
-class add_gate():
-	def __init__(self):
-		print 'add_gate initialized'
-		
-	def forward(self,u0,u1):
-		self.u0 = u0
-		self.u1 = u1
-		self.utop = unit(u0.value+u1.value,0.)
-		return self.utop
-	
-	def backward(self):
-		self.u0.grad += 1 * self.utop.grad
-		self.u1.grad += 1 * self.utop.grad
+def datasetCreation(whatMonth,data,data2,totalN, TrainingToCV=.8):
+	proportions = round(TrainingToCV * totalN)
 
-class sigmoid_gate():
-	def __init__(self):
-		print 'sigmoid_gate initialized'
+	TrainingSet = np.zeros((2,proportions))
+	CrossValidationSet = np.zeros((2,totalN-proportions))
+	
+	count = 0
+	count2 = 0
+	for i in np.arange(1,13):
+		locationsForMonth,=np.where(whatMonth == i)
 		
-	def sigmoid(self,x):
-		return 1./(1.+np.exp(-x))
-	
-	def sigmoid_gradient(self,x):
-		return x*(1.-x)
+		# Shuffle the index locations
+		np.random.shuffle(locationsForMonth)
 		
-	def forward(self,u0):
-		self.u0 = u0
-		self.utop = unit(self.sigmoid(self.u0.value),0.)
+		allotmentForTrainingSet = round(locationsForMonth.size * TrainingToCV)
+		allotmentForCV = locationsForMonth.size - allotmentForTrainingSet
 		
-		return self.utop
-	
-	def backward(self):
-		s = self.sigmoid(self.u0.value)
+		if (count2 + allotmentForCV) > (totalN-proportions):
+			diff = (count2 + allotmentForCV) - (totalN-proportions)
+			allotmentForCV = (totalN-proportions)-count2
+			allotmentForTrainingSet += diff
 		
-		self.u0.grad += self.sigmoid_gradient(s) * self.utop.grad
+		if (count + allotmentForTrainingSet) > (proportions):
+			diff = (count + allotmentForTrainingSet) - (proportions)
+			allotmentForTrainingSet = (proportions)-count
+			allotmentForCV += diff
 		
-	
-def sigmoid(x):
-	return 1./(1.+np.exp(-x))
-def sigmoid_gradient(x):
-	return x*(1.-x)
-def bi_sigmoid(x):
-	return 2./(1.+np.exp(-x)) - 1.
-def bi_sigmoid_gradient(x):
-	return .5*(1.+x)*(1.-x)
-def gaussian_std(x):
-	return (x-np.mean(x))/np.std(x)
-
-def alt_std(x):
-	return (x/np.max(x))*.8 + .1
-def forward_gate(x,y):
-	return x*y
-def forward_gate_add(x,y):
-	return x+y
-def forward_circut(x,y,z):
-	return forward_gate(forward_gate_add(x,y),z)
-
-def alt_std_two(x):
-	return np.min(x),np.max(x)-np.min(x),(x - np.min(x))/(np.max(x)-np.min(x)) 
-
-def H_o(THETA,X):
-	return np.dot(THETA,X)
-	
-def cost_funct(THETA,x,y):
-	hypo = H_o(THETA,x)
-
-	delta = hypo - y
-	#delta_sq = (delta*delta.T)/(2.*delta.size)
-	#print ((hypo * hypo.T)-(y * hypo.T)-(hypo * y.T)+(y * y.T))/(2.*delta.size)
-	
-	return delta#,delta_sq
-
-def batch_gradient_descent(learning_rate,THETA,x,y):
-	
-	delta=cost_funct(THETA,x,y)
-	current_cost = np.dot(delta,delta.T) / (2.*delta.size)
-	THETA_new = THETA.copy()
-	hold_update = np.zeros(THETA.size)
-	
-	for i in np.arange(THETA.size):
-		hold_update[i] = learning_rate * np.dot(delta,x[i,:].T) / (delta.size)
+		locationForTrainingSet = locationsForMonth[:allotmentForTrainingSet]
+		locationForCV = locationsForMonth[allotmentForTrainingSet:]
 		
-	for i in np.arange(THETA.size):
-		THETA_new[0,i] -= hold_update[i]
+		TrainingSet[0,count:count+allotmentForTrainingSet] = data[locationForTrainingSet]
+		TrainingSet[1,count:count+allotmentForTrainingSet] = data2[locationForTrainingSet]
 		
-	return current_cost, THETA_new
+		CrossValidationSet[0,count2:count2+allotmentForCV] = data[locationForCV]
+		CrossValidationSet[1,count2:count2+allotmentForCV] = data2[locationForCV]
+		
+		count += allotmentForTrainingSet
+		count2 += allotmentForCV
+	return TrainingSet, CrossValidationSet
 	
-	
+def parameterDetermination(TrainingSet,polynomalRegress = 2):
+	shapex = TrainingSet.shape[0]
+	shapey = TrainingSet.shape[1]
 
 	
+	Y = TrainingSet[1,:]
+	A = np.ones((1,shapey)) # Accounting for Bias
 	
+	for i in np.arange(1,polynomalRegress+1):
+		powerArr = np.zeros(shapey)+i
+		polynomialX=np.power(TrainingSet[0,:],powerArr)
+		A = np.vstack((A,polynomialX))
+	
+	A = np.matrix(A)
+	Y = np.matrix(Y)
+
+	parameters = np.dot(np.dot(A,A.T).I,np.dot(A,Y.T))
+
+	return parameters
+
+def crossValidationTest(CVSet,parameters,polynomalRegress = 2,test='MSSE'):
+	shapex = CVSet.shape[0]
+	shapey = CVSet.shape[1]
+	n = shapey
+	p = parameters.size
+	
+	Y = CVSet[1,:]
+	A = np.ones((1,shapey)) # Accounting for Bias
+	
+	for i in np.arange(1,polynomalRegress+1):
+		powerArr = np.zeros(shapey)+i
+		polynomialX=np.power(CVSet[0,:],powerArr)
+		A = np.vstack((A,polynomialX))
+	
+	A = np.matrix(A)
+	Y = np.matrix(Y)	
+	
+	YHat = np.dot(parameters.T,A)
+	
+	error = Y - YHat
+	SSE = np.dot(error,error.T)
+	
+	MSE = SSE / n
+	RMSE = np.sqrt(MSE)
+	
+	AIC = n * np.log(SSE) - n * np.log(n) + 2. * p
+	BIC = n * np.log(SSE) - n * np.log(n) + 2. * np.log(p)
+	APC = (n+p)/(n*(n-p)) * SSE
+	
+	return MSE, RMSE, AIC, BIC, APC	
+
 if __name__ == "__main__":
 	
 	import matplotlib.pyplot as plt
 	import numpy as np
-	import netCDF4 as net
 	import sys, os,time
 	import csv,datetime
-	import mpl_toolkits.basemap 
-	from mpl_toolkits.basemap import Basemap
-
-	
 	
 	with open('./montecarloex.csv', 'rb') as csvfile:											# Read in the historical data from NextEra Energy
 		readinfile= csv.reader(csvfile, dialect='excel',quoting=csv.QUOTE_MINIMAL)			
@@ -133,75 +123,82 @@ if __name__ == "__main__":
 				close_p.append(float(row[4]))
 				volume_p.append(float(row[5]))
 
-	ndays_since = np.zeros(np.array(open_p).size)
+
+	totalNumberOfPoints = len(open_p)
+	
+	ndays_since = np.zeros(totalNumberOfPoints)
+	whatMonth = np.zeros(totalNumberOfPoints)
+	
+	# Determine what day this was since the start of the index and what month it is in.
 	for i,date in enumerate(dates):
+		whatMonth[i]=datetime.datetime(int(date[:4]),int(date[5:7]),int(date[8:])).month
 		ndays_since[i]=(datetime.datetime(int(date[:4]),int(date[5:7]),int(date[8:]))-datetime.datetime(int(dates[-1][:4]),int(dates[-1][5:7]),int(dates[-1][8:]))).days
 	
+	# Reverse Arrays For increasing in time
 	ndays_since = ndays_since[::-1]
+	whatMonth = whatMonth[::-1]
+
 	close_p = np.array(close_p)[::-1]
 	open_p = np.array(open_p)[::-1]
 	high_p = np.array(high_p)[::-1]
 	low_p = np.array(low_p)[::-1]
 	volume_m = np.array(volume_p)[::-1]
 	
-	open_p_t = open_p[1:-1]
-	high_p_t = high_p[1:-1]
-	low_p_t = low_p[1:-1]
-	volume_m_t = volume_m[1:-1]
-	ndays_since_t = ndays_since[1:-1]
-	close_p_t = close_p[:-2]
-	close_p_truth = close_p[1:-1]
-	#X_o = np.zeros(ndays_since_t.size)+1.
-	#close_p_truth = np.matrix(close_p_truth)
+	# Determine the number of tests (arbitrary for now)
+	numberOfTests = round(totalNumberOfPoints/10)
 	
+	# Allocate and initialize some arrays
+	estParams = np.matrix(np.zeros((3,numberOfTests)))
+	CVmetric = np.zeros(numberOfTests)
+	CVmetric2 = np.zeros(numberOfTests)
 	
-	min_op,del_op,open_p_t = alt_std_two(open_p_t)
-	min_cl,del_cl,close_p_t = alt_std_two(close_p_t)
-	min_hi,del_hi,high_p_t = alt_std_two(high_p_t)
-	min_lo,del_lo,low_p_t = alt_std_two(low_p_t)
-	min_vol,del_vol,volume_m_t = alt_std_two(volume_m_t)
-	min_ndy,del_ndys,ndays_since_t = alt_std_two(ndays_since_t)
-	min_tru,del_tru,close_p_truth = alt_std_two(close_p[1:-1])
-	close_p_truth = np.matrix(close_p_truth)
-	X_o = np.zeros(ndays_since_t.size)+1.
-	training_data = np.matrix(np.array([X_o,ndays_since_t,open_p_t,close_p_t,high_p_t,low_p_t,volume_m_t]))
+	for testNumber in np.arange(numberOfTests):
 	
-	PARAMS = np.matrix(np.zeros((7)))
+		# Determine the training set and the cross validation set
+		TrainSet, CV = datasetCreation(whatMonth, open_p,close_p,totalNumberOfPoints)
 	
-	test=np.matrix(np.array([X_o[3],ndays_since_t[3],open_p_t[3],close_p_t[3],high_p_t[3],low_p_t[3],volume_m_t[3]]))
-
-	previous_cost = 9999.
-	current_cost = 5
-	dp = 20.
-	i = 0
-	while current_cost>1e-5:
-				
-		current_cost,PARAMS = batch_gradient_descent(1.15,PARAMS,training_data,close_p_truth)
+		# Determine the polynomial regression parameters with Training Set
+		params = parameterDetermination(TrainSet,polynomalRegress = 2)
 		
-		dp = current_cost - previous_cost
+		# Store parameters for later
+		estParams[:,testNumber] = params
+	
+		# Evaluate parameters against a cross-validation set
+		MSETest, RMSETest, AICTest, BICTest, APCTest = crossValidationTest(CV,params,polynomalRegress = 2,test='MSSE')
 		
-		previous_cost = current_cost
-		if i % 10 == 0:
-			print current_cost
-		
-		i+=1
+		# Store some of the metrics in a holding array
+		CVmetric[testNumber] = AICTest
+		CVmetric2[testNumber] = MSETest
+	
+	# Find the minimum error metric (best scenario)
+	bestEstimate, = np.where(CVmetric == np.nanmin(CVmetric))
+	bestParameters = estParams[:,bestEstimate] 
+	
+	# Output
+	print "The best parameters are (a_o + a_1 x + a_2 x^2):"
+	print "		a_o =", bestParameters[0,:]
+	print "		a_1 =", bestParameters[1,:]
+	print "		a_2 =", bestParameters[2,:]
+	
+	fig = plt.figure(figsize=(10,10))
+	ax = fig.add_subplot(211)
+	ax.plot(CVmetric)
+	ax.plot(bestEstimate,CVmetric[bestEstimate],'o')
+	ax.set_xlim([0,CVmetric.size])
+	ax.set_xlabel("Random Fold Number")
+	ax.set_ylabel("AIC")
+	ax.set_title("Akaike's Information Criterion")
 
-	
-	dp=np.dot(PARAMS,training_data)-close_p_truth
-	
-	# NORMAL EQUATIONS (1 step, no iterations!)	
-	PARAMS2 = np.dot(np.dot((np.dot(training_data,training_data.T)).I,training_data),close_p_truth.T).T
-	
-
-	gradient_descent_est = np.dot(PARAMS,training_data).T
-	normal_eqs_est = np.dot(PARAMS2,training_data).T
+	ax = fig.add_subplot(212)
+	ax.plot(CVmetric2)
+	ax.set_xlim([0,CVmetric2.size])
+	ax.plot(bestEstimate,CVmetric2[bestEstimate],'o')
+	ax.set_xlabel("Random Fold Number")
+	ax.set_ylabel("MSE")
+	ax.set_title("Mean Squared Error")
 	
 	
-	
-	plt.plot(normal_eqs_est-close_p_truth.T)
-	#plt.plot(close_p_truth.T)
 	plt.show()
-	
 	sys.exit()
 
 		
